1edcba26c4bb5e0e421b28b551a71aa8f313003f
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2026-05-03 16:32:45 +0700

    up

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -14,12 +14,9 @@ if (PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    trigger_error(
-        $err,
-        E_USER_ERROR
-    );
+    throw new RuntimeException($err);
 }
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitb3e4a427f5c94e0f9a18aba5efe988a4::getLoader();
+return ComposerAutoloaderInita271541b7461cf0e4471962783e28dda::getLoader();
